d9f70ebbdd09126ff5db04aeaf9b4e6eeec203e5
BreakwebMkt <grazi.ds02@gmail.com>
2023-10-26 12:17:52 -0300

    ideia para tipos de stands

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,17 @@
-const image = document.getElementById('image');
-const text = document.getElementById('text');
-let currentIndex = 0;
+const optionButtons = document.querySelectorAll('.option-button');
+const options = document.querySelectorAll('.option');
 
-const textList = [
-    { imageSrc: 'images/ng-gas.JPG', textContent: 'Confeccionados com materiais padrões (Octanorm) ou vidro, o Stand do tipo Padrão, traz ao nosso cliente a possibilidade de trabalhar em uma feira ou evento de forma simples e eficaz.' },
-    { imageSrc: 'images/web-spot.JPG', textContent: 'Possui uma combinação de elementos construídos e padrões, apresentando versatilidade em sua criação, agilidade na sua montagem e personalização conforme a necessidade do cliente.' },
-    { imageSrc: 'images/viraj.JPG', textContent: 'Com seus formatos, materiais e cores, cada estrutura é cuidadosamente projetada para ser única e sob medida para o evento em questão, garantindo a certeza de uma presença marcante e memorável.' },
-];
+options.forEach(option => option.style.display = 'none');
 
-function updateContent() {
-    const currentText = textList[currentIndex];
-    image.src = currentText.imageSrc;
-    text.textContent = currentText.textContent;
-}
+optionButtons.forEach(button => {
+  button.addEventListener('mouseover', () => {
+    const selectedOption = button.getAttribute('data-option');
+    options.forEach(option => option.style.display = 'none');
+    document.getElementById(selectedOption).style.display = 'block';
+  });
 
-const buttons = document.querySelectorAll('.option');
-
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    buttons.forEach(otherButton => {
-      if (otherButton !== button) {
-        otherButton.classList.remove('clicked');
-      }
-    });
-    button.classList.add('clicked');
+  button.addEventListener('mouseout', () => {
+    const selectedOption = button.getAttribute('data-option');
+    document.getElementById(selectedOption).style.display = 'none';
   });
 });
-
-updateContent();
-
